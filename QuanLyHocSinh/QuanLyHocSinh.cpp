89da0b753728cdbfc3287e3b85e9ab31c4d3a242
khanhvu1997 <khanhvu0397@gmail.com>
2020-05-25 09:12:13 +0700

    C++

---
+++ QuanLyHocSinh/QuanLyHocSinh.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include "student.h"
+using namespace std;
+
+int main()
+{
+	Person *person;
+	int input;
+	while (true) {
+		if (!x && !y) {
+			cout << "\n----------------------------------------";
+			cout << "\n+++++Chuong trinh quan ly truong hoc+++++\n";
+			cout << "1: Hoc sinh.\n";
+			cout << "2: Giao vien.\n";
+			cout << "0: Thoat.\n";
+			input = person->getIntRange(0, 2);
+
+			switch (input)
+			{
+			case 1: 
+				person->student();
+				break;
+
+			case 2: 
+				person->teacher();
+				break;
+
+			case 0: 
+				return 0;
+			}
+		}
+		else if (!y ) {
+			cout << "\n----------------------------------------";
+			cout << "\n+++++Chuong trinh quan ly hoc sinh+++++\n";
+			cout << "1: Hien thi danh sach hoc sinh.\n";
+			cout << "2: Them hoc sinh.\n";
+			cout << "3: Xoa hoc sinh.\n";
+			cout << "4: Sua hoc sinh.\n";
+			cout << "0: Thoat chuong trinh hoc sinh.\n";
+			person = new Student;
+			input = person->getIntRange(0, 4);
+			switch (input) {
+			case 1:
+				cout << "Danh sach hoc sinh:\n";
+				person->list();
+				break;
+			case 2:
+				cout << "Them hoc sinh:\n";
+				person->add();
+				person->saveFile();
+				cout << "Them thanh cong!";
+				break;
+			case 3:
+				cout << "Xoa hoc sinh:\n";
+				person->remove();
+				person->saveFile();
+				cout << "Xoa thanh cong!";
+				break;
+			case 4:
+				cout << "Sua hoc sinh:\n";
+				person->update();
+				person->saveFile();
+				cout << "Sua thanh cong!";
+				break;
+			case 0:
+				person->exit();
+				break;
+			}
+		}
+		else {
+			cout << "\n----------------------------------------";
+			cout << "\n+++++Chuong trinh quan ly giao vien+++++\n";
+			cout << "1: Hien thi danh sach giao vien.\n";
+			cout << "2: Them giao vien.\n";
+			cout << "3: Xoa giao vien.\n";
+			cout << "4: Sua giao vien.\n";
+			cout << "0: Thoat chuong trinh giao vien.\n";
+			person = new Teacher;
+			input = person->getIntRange(0, 4);
+			switch (input) {
+			case 1:
+				cout << "Danh sach giao vien:\n";
+				person->list();
+				break;
+			case 2:
+				cout << "Them giao vien:\n";
+				person->add();
+				person->saveFile();
+				cout << "Them thanh cong!";
+				break;
+			case 3:
+				cout << "Xoa giao vien:\n";
+				person->remove();
+				person->saveFile();
+				cout << "Xoa thanh cong!";
+				break;
+			case 4:
+				cout << "Sua giao vien:\n";
+				person->update();
+				person->saveFile();
+				cout << "Sua thanh cong!";
+				break;
+			case 0:
+				person->exit();
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
---
+++ QuanLyHocSinh/student.cpp
@@ -0,0 +1,149 @@
+#include "student.h"
+#include <iostream>
+#include <fstream>
+
+
+
+//Class Person
+	int Person::getIntRange(int low, int high) {
+		int input;
+		while (true) {
+			cin >> input;
+			if (input < low || input > high) {
+				cout << "Gia tri khong hop le. Vui long nhap lai!\n";
+			}
+			else
+				break;
+		}
+		return input;
+	}
+
+	void Person::inputInfo(int id)
+	{
+		cout << "Nhap ma: \n";
+		cin >> id;
+		cout << "Nhap ten: \n";
+		cin >> name;
+		cout << "Nhap tuoi: \n";
+		cin >> age;
+	}
+
+	void Person::printInfo(int id)
+	{
+		cout << "\n---------------\n";
+		cout << "Ma la: " << id;
+		cout << "\nTen la: " << name;
+		cout << "\nTuoi la: " << age;
+	}
+	void Person::saveFile()
+	{
+		ofstream output;
+		output.open(fileName);
+		for (int i = 1; i <= number; i++)
+		{
+			output << id << '\n';
+			output << name << '\n';
+			output << age << '\n';
+		}
+		output.close();
+	}
+	void Person::student() {
+		x = true;
+	}
+	void Person::teacher() {
+		y = true;
+		x = false;
+	}
+	void Person::exit() {
+		x = false;
+		y = false;
+	}
+
+//Class Student
+	void Student::add()
+	{
+		number++;
+		Person::inputInfo(number);
+		cout << "Nhap nghe:";
+		cin >> jobStu;
+	}
+	void Student::list()
+	{
+		for (int i = 1; i <= number; i++) {
+			Person::printInfo(i);
+			cout << "Nghe nghiep la:" << jobStu;
+		}
+			
+	}
+
+	void Student::remove()
+	{
+		int id;
+		cout << "Hay nhap ma so hoc sinh: ";
+		cin >> id;
+		if (id < 1 || id > number)
+		{
+			cout << "Ma so hoc sinh khong hop le!\n";
+			return;
+		}
+		for (int i = id + 1; i <= number; i++)
+			//allStudent[i - 1] = allStudent[i];
+		number--;
+	}
+	void Student::update()
+	{
+		int id;
+		cout << "Hay nhap ma so hoc sinh: ";
+		cin >> id;
+		if (id < 1 || id > number)
+		{
+			cout << "Ma so hoc sinh khong hop le!\n";
+			return;
+		}
+		Person::inputInfo(id);
+	}
+
+//Class Teacher
+
+
+	void Teacher::add() {
+		number++;
+		Person::inputInfo(number);
+		cout << "Nghe nghiep la:";
+		cin >> jobTea;
+	}
+	void Teacher::list()
+	{
+		for (int i = 1; i <= number; i++) {
+			Person::printInfo(i);
+			cout << "Nghe nghiep la:" << jobTea;
+		}		
+	}
+
+	void Teacher::remove()
+	{
+		int id;
+		cout << "Hay nhap ma giao vien: ";
+		cin >> id;
+		if (id < 1 || id > number)
+		{
+			cout << "Ma giao vien khong hop le!\n";
+			return;
+		}
+		for (int i = id + 1; i <= number; i++)
+			//allStudent[i - 1] = allStudent[i];
+			number--;
+	}
+	void Teacher::update()
+	{
+		int id;
+		cout << "Hay nhap ma giao vien: ";
+		cin >> id;
+		if (id < 1 || id > number)
+		{
+			cout << "Ma giao vien khong hop le!\n";
+			return;
+		}
+		Person::inputInfo(id);
+	}
+
